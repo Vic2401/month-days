f319db1f0935980a4bf83c8bcae9b8659f34095b
Viktor Burman <vburman2401@gmail.com>
2021-02-06 19:06:50 +0300

    06.02.21

--- app.js
+++ app.js
@@ -20,41 +20,70 @@
 // const result = Math.floor( Math.random()*10);
 // const result = Math.ceil( Math.random()*10);
 
-const months = [
-'January', 
-'February', 
-'March', 
-'April', 
-'May', 
-'June', 
-'July', 
-'August',
-'September',
-'October',
-'November', 
-'December', ];
+// const months = [
+// 'January', 
+// 'February', 
+// 'March', 
+// 'April', 
+// 'May', 
+// 'June', 
+// 'July', 
+// 'August',
+// 'September',
+// 'October',
+// 'November', 
+// 'December', ];
 
-const days = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-];
-
-
-const date = new Date();
-const month = date.getMonth();
-// console.log(months[month]);
+// const days = [
+//     'Sunday',
+//     'Monday',
+//     'Tuesday',
+//     'Wednesday',
+//     'Thursday',
+//     'Friday',
+//     'Saturday',
+// ];
 
 
-const day = date.getDay();
-// console.log(days[day]);
+// const date = new Date();
+// const month = date.getMonth();
+// // console.log(months[month]);
 
-// console.log(date.getDate());
-// console.log(date.getFullYear());
 
-const sentence = `${days[day]}, ${date.getDate()}th ${months[month]} ${date.getFullYear()}`;
-console.log(sentence);
+// const day = date.getDay();
+// // console.log(days[day]);
+
+// // console.log(date.getDate());
+// // console.log(date.getFullYear());
+
+// const sentence = `${days[day]}, ${date.getDate()}th ${months[month]} ${date.getFullYear()}`;
+// console.log(sentence);
+
+// const h1 = document.getElementById('title');
+// h1.style.color = 'red';
+// const btn = document.getElementById('btn');
+
+// btn.style.backgroundColor = 'blue';
+// btn.style.color = 'white';
+
+
+// const headings = document.getElementsByTagName('h1');
+// console.log(headings);
+// headings[1].style.color = 'green';
+// console.log(headings.length);
+
+// const items = document.getElementsByTagName('li');
+// items[1].style.color = 'green';
+// const betterItems = [...items];
+
+// betterItems.forEach(function(item){
+//     // console.log(item);
+// });
+// console.log(items);
+// console.log(betterItems);
+
+// const listItems = document.getElementsByClassName('special');
+// console.log(listItems);
+// listItems[0].style.color = 'blue';
+// const result = document.querySelector('#result');
+// result.style.backgroundColor = 'blue';
